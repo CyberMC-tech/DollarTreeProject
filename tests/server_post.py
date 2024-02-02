a62ff25d635fb24fc1c732ab6f08551e4ecfd697
Mathew Conder <mconder@cybermc.tech>
2024-02-02 13:58:43 -0500

    Update server_post.py

--- tests/server_post.py
+++ tests/server_post.py
@@ -1,7 +1,7 @@
 import requests
 
 url = "http://127.0.0.1:5000/get_employee_info"
-data = {"username": "27606069", "password": "5422"}
+data = {"username": "", "password": ""}
 
 response = requests.post(url, json=data)
 print(response.text)
